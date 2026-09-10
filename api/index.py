7fc589af8d7cc19e8d56847487f775bf86e4dd14
Jeam87 <88300558+Jeam87@users.noreply.github.com>
2026-09-09 23:33:44 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -302,4 +302,4 @@ function actualizarClienteTicket(){ let sel=document.getElementById('selCliente'
 </script></body></html>
 """
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=5000)
