38f0237bcf51fa6d24c6d3fb6e3ed1ceedd4d75b
Jeam87 <88300558+Jeam87@users.noreply.github.com>
2026-09-25 11:30:07 -0600

    Change return format to jsonify for payment link

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def crear_link_cobro():
     data = request.get_json()
     monto = int(float(data.get('monto',0))*100)
     link = stripe.PaymentLink.create(line_items=[{"price_data":{"currency":"mxn","product_data":{"name":data.get('concepto','Botes Jacona')},"unit_amount":monto},"quantity":1}])
-    return {"url": link.url}
+    return jsonify({"url": link.url})
 
 @app.route('/manifest.json')
 def manifest():
